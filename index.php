b62f590087e65c32437a2805f82698518ee8ddb9
SnakeKidsLeader <SweetClown@users.noreply.github.com>
2016-03-03 02:33:45 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+echo "<DOCTYPE html>
+<html>
+<head>
+	<link
+	rel ='stylesheet'
+	href = 'style.css'>
+
+<title>ASP</title>
+</head>
+
+<body>
+
+<center><h1>Welcome To Python Web Server</h1></center>
+
+
+
+<center><h2>this is python server with PHP5 support</h2></center>
+
+
+
+
+
+
+
+
+<p><footer><center><font color = blue>Powered B</font><font color = yellow>y Python 2.7</center></font>
+<center>AESPro Present</p></center>
+
+
+</body>
+</html>"
+
+?>
